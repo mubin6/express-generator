091eb20884fef27acabae907da6672e5f7525abc
mubin <mubin@kisanforum.org>
2019-07-13 21:18:18 +0530

    express session part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -35,11 +37,18 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-6789-09876-54321'));
+// app.use(cookieParser('12345-6789-09876-54321'));
+app.use(session({
+    name: 'session-id',
+    secret: '12345-6789-09876-54321',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+}));
 
 function auth(req, res, next) {
-    console.log(req.signedCookies);
-    if (!req.signedCookies.user) {
+    console.log(req.session);
+    if (!req.session) {
         var authHeader = req.headers.authorization;
 
         if (!authHeader) {
@@ -54,7 +63,8 @@ function auth(req, res, next) {
         var password = auth[1];
 
         if (username == 'admin' && password == 'password') {
-            res.cookie('user', 'admin', { signed: true });
+            // res.cookie('user', 'admin', { signed: true });
+            req.session.user = 'admin';
             next();
         } else {
             var err = new Error('You are not authenticated');
@@ -63,7 +73,7 @@ function auth(req, res, next) {
             return next(err);
         }
     } else {
-        if (req.signedCookies.user === 'admin') {
+        if (req.session.user === 'admin') {
             next();
         } else {
             var err = new Error('You are not authenticated');
