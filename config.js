a7489a8a351a3c43b84bd28c96fad3ba348c5edb
mubin <mubin@kisanforum.org>
2020-04-21 18:24:02 +0530

    passport-facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1303272863200709',
+        clientSecret: '3625ce434ad2def6f033845f764cfc8c'
+    }
 }
